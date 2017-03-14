95d21d0f9c8c4be2fd97dc0715998acd7d5f3a0b
taptipblard <francisabelard@gmail.com>
2017-03-14 10:50:25 +0800

    add output file for single post

---
+++ single.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+<!-- INDEX: End of Header  -->
+		<!-- INDEX: Start of Content  -->
+		<section class="single-content container page-wrap">
+        	<div class="row">
+        		<div class="col-sm-offset-2 col-sm-8 col-xs-12">
+        		<a href="<?php echo  esc_url( home_url( '/blog' ) );?>" class="btn btn-block float-right"><span class="glyphicon glyphicon-menu-left"></span>Back</a>
+				<?php
+				if (have_posts()) {
+					while (have_posts()) {
+					the_post();
+					$post_thumbnail_id = get_post_thumbnail_id($post->ID);
+					$post_thumbnail_url = wp_get_attachment_url( $post_thumbnail_id );
+				?>
+
+					<h2 class="blog-post-title"><?php the_title(); ?></h2>
+					<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+					<hr>
+					<?php the_content(); ?>
+				<?php 
+					}
+				}
+				?>
+				</div>
+			</div>
+		</section>
+		<!-- INDEX: End of Content  -->
+		<!-- INDEX: Start of Sidebar  -->
+		<?php 
+		// get_sidebar(); 
+		?>
+		<!-- INDEX: End of Sidebar  -->
+<!-- INDEX: Start of Content  -->
+<?php get_footer(); ?>
